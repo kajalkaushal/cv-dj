66f2b2e727fd340875af5ea9a9a54f42695e89ff
KAJAL KAUSHAL <121794528+kajalkaushal@users.noreply.github.com>
2023-08-24 15:58:20 +0530

    Update wsgi.py

--- mycv/wsgi.py
+++ mycv/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'folio.settings')
 
 application = get_wsgi_application()
 
