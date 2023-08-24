78b0136ecce4927ca182723bdeb2558c77700dda
KAJAL KAUSHAL <121794528+kajalkaushal@users.noreply.github.com>
2023-08-24 15:55:33 +0530

    Update urls.py

--- mycv/urls.py
+++ mycv/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mycv import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
