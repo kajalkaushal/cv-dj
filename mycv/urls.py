5c6ed9b1fcbeb9eea095e14ca51b8de1f5d51197
KAJAL KAUSHAL <121794528+kajalkaushal@users.noreply.github.com>
2023-08-24 15:54:51 +0530

    Update urls.py

--- mycv/urls.py
+++ mycv/urls.py
@@ -22,4 +22,5 @@ from mycv import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index )
-]
+]urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
